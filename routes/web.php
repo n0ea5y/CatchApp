62fcac7f46958fbe04738620346159ebb14ad48f
n0ea5y <n0ea5y2024@gmail.com>
2024-12-14 01:33:03 +0900

    #1 ルート関連のpush忘れ

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,50 @@
 <?php
 
+use App\Http\Controllers\Admin\Auth\AdminAuthenticatedSessionController;
+use App\Http\Controllers\EndUser\Auth\EndUserAuthenticatedSessionController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('login', [AdminAuthenticatedSessionController::class, 'create'])->name('login');
+    Route::post('login', [AdminAuthenticatedSessionController::class, 'store'])->name('login.store');
+
+    Route::middleware('auth:admin')->group(function() {
+        Route::get('dashboard', function () {
+            return Inertia::render('Dashboard');
+        })->name('dashboard');
+
+        Route::get('shift-management', function () {
+            return Inertia::render('Admin/Shift/ShiftManagement');
+        })->name('shift.index');
+
+        Route::get('staff-management', function () {
+            return Inertia::render('Admin/Staff/StaffManagement');
+        })->name('staff.index');
+
+        Route::get('store-management', function () {
+            return Inertia::render('Admin/Store/StoreManagement');
+        })->name('store.index');
+
+        Route::post('logout', [AdminAuthenticatedSessionController::class, 'destroy'])->name('logout');
+    });
+
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::prefix('enduser')->name('enduser.')->group(function () {
+    Route::get('login', [EndUserAuthenticatedSessionController::class, 'create'])->name('login');
+    Route::post('login', [EndUserAuthenticatedSessionController::class, 'store'])->name('login.store');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::middleware('auth:enduser')->group(function() {
+        Route::get('dashboard', function () {
+            return Inertia::render('Dashboard');
+        })->name('dashboard');
+
+        Route::post('logout', [EndUserAuthenticatedSessionController::class, 'destroy'])->name('logout');
+    });
+
 });
 
-require __DIR__.'/auth.php';
+// require __DIR__.'/auth.php';
